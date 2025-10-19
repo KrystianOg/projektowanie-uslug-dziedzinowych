3823f91d37b3e7102139c8d0a51eb9a77468bf42
KrystianOg <krystian@ogonowski.it>
2025-10-19 20:09:51 +0200

    basic api gateway

---
+++ services/api_gateway/main.py
@@ -0,0 +1,24 @@
+from typing import Union
+
+from fastapi import FastAPI
+from fastapi import WebSocket
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_text(f"Message text was: {data}")
